046ceb4f5c5c90f3791a567fc1d94e93ec7b0210
Jon Sowers <jon.sowers@skydio.com>
2022-10-01 11:15:37 -0700

    Update nvim

--- .config/nvc-custom/chadrc.lua
+++ .config/nvc-custom/chadrc.lua
@@ -7,59 +7,100 @@ local M = {}
 -- example of changing theme:
 
 M.options = {
-  mapleader = ",",
-  numberwidth = 2,
-  relativenumber = false,
-  expandtab = true,
-  shiftwidth = 2,
-  smartindent = true,
-  tabstop = 2,
+  user = function()
+    vim.opt.number = false
+    vim.g.mapleader = ","
+    vim.opt.numberwidth = 2
+    vim.opt.relativenumber = false
+    vim.opt.expandtab = true
+    vim.opt.shiftwidth = 2
+    vim.opt.tabstop = 2
+  end,
 }
 
 M.ui = {
-   theme = "chadracula",
+  theme = "chadracula",
+  statusline = {
+    separator_style = "round",
+  },
 }
 
 M.mappings = {
-  misc = {
-    line_number_toggle = "<space>n"
+  general = {
+    n = {
+      ["<space>n"] = { "<cmd> set nu! <CR>", "   toggle line number" },
+			["<leader>ft"] = { ":Telescope treesitter<CR>", "toggle telescope for treesitter"},
+			["<leader>gb"] = { ":Gitsigns toggle_current_line_blame<CR>", "Toggle git blame"},
+			["<leader>gr"] = { ":Telescope lsp_references<CR>", "toggle telescope for lsp references"},
+    }
+  },
+  nvimtree = {
+    n = {
+      -- toggle
+      ["<leader>n"] = { "<cmd> NvimTreeToggle <CR>", "   toggle nvimtree" },
+      -- focus
+      ["<leader>e"] = { "<cmd> NvimTreeFocus <CR>", "   focus nvimtree" },
+    },
+  },
+  bufferline = {
+    n = {
+      -- new buffer
+      ["<S-b>"] = { "<cmd> enew <CR>", "烙 new buffer" },
+
+      -- cycle through buffers
+      ["<C-n>"] = { "<cmd> BufferLineCycleNext <CR>", "  cycle next buffer" },
+      ["<C-p>"] = { "<cmd> BufferLineCyclePrev <CR>", "  cycle prev buffer" },
+
+      -- close buffer + hide terminal buffer
+      ["<leader>q"] = {
+         function()
+            require("core.utils").close_buffer()
+         end,
+         "   close buffer",
+      },
+    },
   }
 }
 
-M.mappings.plugins = {
-  bufferline = {
-    next_buffer = "<C-n>",
-    prev_buffer = "<C-p>",
-  },
-  nvimtree = {
-    toggle = "<leader>n"
-  }
-}
 
 M.plugins = {
   options = {
     lspconfig = {
       setup_lspconf = "custom.configs.lsp_configs"
     },
-    nvimtree = {
-       enable_git = 1,
-       -- packerCompile required after changing lazy_load
-       lazy_load = true,
-
-       ui = {
-          allow_resize = true,
-          side = "left",
-          width = "20%",
-          auto_resize = true,
-          hide_root_folder = false,
-       },
-    },
   },
-  default_plugin_config_replace = {
-    lspconfig = "custom.configs.lspconfig",
-    nvim_tree = "custom.configs.nvimtree",
-    gitsigns = "custom.configs.gitsigns",
-    lsp_handlers = "custom.configs.lsp_handlers"
+  override = {
+    ["kyazdani42/nvim-tree.lua"] = {
+      renderer = { 
+        git = {
+          enable = true,
+        }
+      },
+      view = {
+         side = "left",
+         width = "20%",
+         hide_root_folder = true,
+      },
+      actions = {
+        open_file = {
+          quit_on_open = true,
+        },
+      }
+    }
+  },
+  user = {
+    ["ur4ltz/surround.nvim"] = {
+      config = function()
+        require("surround").setup {mappings_style = "sandwich"}
+      end
+    },
+    ["lambdalisue/suda.vim"] = {},
+    ["jose-elias-alvarez/null-ls.nvim"] = {
+      after = "nvim-lspconfig",
+      config = function()
+         require("custom.configs.null-ls").setup()
+      end
+    },
   },
 }
 
--- .config/nvc-custom/init.lua
+++ .config/nvc-custom/init.lua
@@ -6,12 +6,12 @@
 
 -- MAPPINGS
 -- To add new mapps, use the "setup_mappings" hook,
-local map = require("core.utils").map
-
- map("n", "<leader>q", ":bd<CR>", opt)
- map("n", "<leader>ft", ":Telescope treesitter<CR>")
- map("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>")
- map("n", "<leader>gr", ":Telescope lsp_references<CR>")
+-- local map = require("core.utils").map
+-- 
+--  map("n", "<leader>q", ":bd<CR>", opt)
+--  map("n", "<leader>ft", ":Telescope treesitter<CR>")
+--  map("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>")
+--  map("n", "<leader>gr", ":Telescope lsp_references<CR>")
 
 -- NOTE : opt is a variable  there (most likely a table if you want multiple options),
 -- you can remove it if you dont have any custom options
@@ -20,25 +20,25 @@ local map = require("core.utils").map
 -- To add new plugins, use the "install_plugin" hook,
 
 -- examples below:
-local customPlugins = require("core.customPlugins")
-customPlugins.add(function(use)
-   use {
-     "ur4ltz/surround.nvim",
-      config = function()
-        require("surround").setup {mappings_style = "sandwich"}
-      end
-   }
-   use {
-     "lambdalisue/suda.vim"
-   }
-   use {
-      "jose-elias-alvarez/null-ls.nvim",
-      after = "nvim-lspconfig",
-      config = function()
-         require("custom.configs.null-ls").setup()
-      end
-   }
-end)
+-- local customPlugins = require("core.customPlugins")
+-- customPlugins.add(function(use)
+--    use {
+--      "ur4ltz/surround.nvim",
+--       config = function()
+--         require("surround").setup {mappings_style = "sandwich"}
+--       end
+--    }
+--    use {
+--      "lambdalisue/suda.vim"
+--    }
+--    use {
+--       "jose-elias-alvarez/null-ls.nvim",
+--       after = "nvim-lspconfig",
+--       config = function()
+--          require("custom.configs.null-ls").setup()
+--       end
+--    }
+-- end)
 
 -- NOTE: we heavily suggest using Packer's lazy loading (with the 'event' field)
 -- see: https://github.com/wbthomason/packer.nvim
